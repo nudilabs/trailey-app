fd79871a7f8a8b626aa0d401ddd02deebd616722
ayvee <asianvikingofficial@gmail.com>
2023-04-08 09:48:48 +0700

    ✨ (Layout): add loading progress bar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,7 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
+import { Progress } from '@chakra-ui/react';
 import SidebarWithHeader from './SidebarWithHeader';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -11,12 +13,40 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       ? 'Home'
       : pathname.charAt(1).toUpperCase() + pathname.slice(2);
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = () => setIsLoading(true);
+    const handleComplete = () => setIsLoading(false);
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  }, [router]);
+
   return (
     <>
       <Helmet>
         <title>{pageTitle}</title>
         {/* <meta name="description" content="Your page description" /> */}
       </Helmet>
+      {isLoading && (
+        <Progress
+          size="xs"
+          colorScheme="pink"
+          isIndeterminate
+          position="fixed"
+          top="0"
+          left="0"
+          right="0"
+        />
+      )}
       <SidebarWithHeader>
         <main>{children}</main>
       </SidebarWithHeader>
