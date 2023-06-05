a2ec290a0f2f3e7b29759ab161dcf28f59f4265f
ayvee <asianvikingofficial@gmail.com>
2023-06-05 13:47:03 +0700

    ✨ (iOS mobile): remove search bar when save to home

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
